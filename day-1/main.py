5bff59604103ef2afacad43a745cb2225404ffd3
Pieter Polmans <pieter.polmans@protonmail.com>
2023-12-01 14:44:03 +0100

    day 1
    Signed-off-by: Pieter Polmans <pieter.polmans@protonmail.com>

---
+++ day-1/main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import re
+
+INPUT_FILE = 'input.txt'
+
+def get_input():
+    lines = []
+    with open('input.txt') as f:
+        lines = f.readlines()
+        lines = [ line[:-1] for line in lines ]
+
+    return lines
+
+
+def get_first_and_last_number(string: str):
+    first_num = int(re.search(r'\d{1}', string).group())
+    last_num = int(re.search(r'\d{1}', string[::-1]).group())
+
+    return (first_num, last_num)
+
+
+def combine_number_pair(number_pair: tuple):
+    return number_pair[0]*10 + number_pair[1]
+
+
+def main():
+    lines = get_input()
+    pairs = [get_first_and_last_number(line) for line in lines]
+    combined_numbers = [combine_number_pair(pair) for pair in pairs]
+    sum_of_combined_numbers = sum(combined_numbers)
+    print(sum_of_combined_numbers)
+
+
+if __name__ == '__main__':
+    main()
